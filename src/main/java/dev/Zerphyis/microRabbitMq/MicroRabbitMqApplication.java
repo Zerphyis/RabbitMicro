28ed9e944e49971e42255609dd217ccb6a0347bc
Otavio <estudosprogotavio@gmail.com>
2025-09-09 00:33:14 -0300

    feat:commit inicial

---
+++ src/main/java/dev/Zerphyis/microRabbitMq/MicroRabbitMqApplication.java
@@ -0,0 +1,13 @@
+package dev.Zerphyis.microRabbitMq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroRabbitMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroRabbitMqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/Zerphyis/microRabbitMq/MicroRabbitMqApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.Zerphyis.microRabbitMq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroRabbitMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
