3c345730fae0d0470eb211961ae8751bf6d527a9
Otavio <estudosprogotavio@gmail.com>
2025-09-10 12:28:33 -0300

    TASK05- feat:adicionando ProductMapper

---
+++ src/main/java/dev/Zerphyis/microRabbitMq/Application/mapper/ProductMapper.java
@@ -0,0 +1,33 @@
+package dev.Zerphyis.microRabbitMq.Application.mapper;
+
+import dev.Zerphyis.microRabbitMq.Application.dto.ProductRequestDto;
+import dev.Zerphyis.microRabbitMq.Application.dto.ProductResponseDto;
+import dev.Zerphyis.microRabbitMq.Domain.model.Product;
+
+public class ProductMapper {
+    public static Product toEntity(ProductRequestDto dto) {
+        if (dto == null) return null;
+
+        return Product.builder()
+                .name(dto.getName())
+                .description(dto.getDescription())
+                .price(dto.getPrice())
+                .stock(dto.getStock())
+                .category(dto.getCategory())
+                .build();
+    }
+
+    public static ProductResponseDto toResponse(Product product) {
+        if (product == null) return null;
+
+        return ProductResponseDto.builder()
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .stock(product.getStock())
+                .category(product.getCategory())
+                .createdAt(product.getCreatedAt())
+                .updateAt(product.getUpdatedAt())
+                .build();
+    }
+}
