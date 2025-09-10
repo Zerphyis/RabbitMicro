e36d628da2937a4afbf324e3540d143d1fe4d6be
Otavio <estudosprogotavio@gmail.com>
2025-09-10 12:45:41 -0300

    TASK08- refactor:adicionando metodo findAllWithFilters em ProductRepositoryImpl

--- src/main/java/dev/Zerphyis/microRabbitMq/Domain/repository/ProductRepositoryImpl.java
+++ src/main/java/dev/Zerphyis/microRabbitMq/Domain/repository/ProductRepositoryImpl.java
@@ -2,7 +2,12 @@ package dev.Zerphyis.microRabbitMq.Domain.repository;
 
 import dev.Zerphyis.microRabbitMq.Domain.model.Product;
 import dev.Zerphyis.microRabbitMq.Domain.repository.jpa.ProductRepositoryJpa;
+import dev.Zerphyis.microRabbitMq.Domain.spefication.ProductSpecification;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -33,5 +38,11 @@ public class ProductRepositoryImpl implements ProductRepository{
     @Override
     public void deleteId(UUID id) {
         jpaRepository.deleteById(id);
+    }
+
+    @Override
+    public Page<Product> findAllWithFilters(String name, String category, BigDecimal minPrice, BigDecimal maxPrice, Pageable pageable) {
+        Specification<Product> spec = ProductSpecification.filterBy(name, category, minPrice, maxPrice);
+        return jpaRepository.findAll(spec, pageable);
     }
 }
