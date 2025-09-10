fe682c68021207f8b929a3f9ee4c04bf453660ec
Otavio <estudosprogotavio@gmail.com>
2025-09-10 12:44:42 -0300

    TASK08- feat:classe ProductSpecification

---
+++ src/main/java/dev/Zerphyis/microRabbitMq/Domain/spefication/ProductSpecification.java
@@ -0,0 +1,28 @@
+package dev.Zerphyis.microRabbitMq.Domain.spefication;
+
+import dev.Zerphyis.microRabbitMq.Domain.model.Product;
+import jakarta.persistence.criteria.Predicate;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.math.BigDecimal;
+
+public class ProductSpecification {
+    public static Specification<Product> filterBy(String name, String category, BigDecimal minPrice, BigDecimal maxPrice) {
+        return (root, query, cb) -> {
+            Predicate predicate = cb.conjunction();
+            if (name != null && !name.isBlank()) {
+                predicate = cb.and(predicate, cb.like(cb.lower(root.get("name")), "%" + name.toLowerCase() + "%"));
+            }
+            if (category != null && !category.isBlank()) {
+                predicate = cb.and(predicate, cb.equal(root.get("category"), category));
+            }
+            if (minPrice != null) {
+                predicate = cb.and(predicate, cb.greaterThanOrEqualTo(root.get("price"), minPrice));
+            }
+            if (maxPrice != null) {
+                predicate = cb.and(predicate, cb.lessThanOrEqualTo(root.get("price"), maxPrice));
+            }
+            return predicate;
+        };
+    }
+}
