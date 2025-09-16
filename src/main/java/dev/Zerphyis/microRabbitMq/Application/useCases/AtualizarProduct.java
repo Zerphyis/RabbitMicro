9de67921cff539aab40a06b9a0a461c6d95f8038
Otavio <estudosprogotavio@gmail.com>
2025-09-16 04:09:36 -0300

    TASK10- feat:adicionando caso de uso de AtualizarProduct

---
+++ src/main/java/dev/Zerphyis/microRabbitMq/Application/useCases/AtualizarProduct.java
@@ -0,0 +1,30 @@
+package dev.Zerphyis.microRabbitMq.Application.useCases;
+
+import dev.Zerphyis.microRabbitMq.Application.dto.ProductRequestDto;
+import dev.Zerphyis.microRabbitMq.Domain.model.Product;
+import dev.Zerphyis.microRabbitMq.Domain.repository.ProductRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public class AtualizarProduct {
+    private final ProductRepository productRepository;
+
+    public AtualizarProduct(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Optional<Product> execute(UUID id, ProductRequestDto dto) {
+        Optional<Product> existingProduct = productRepository.findByid(id);
+        if (existingProduct.isPresent()) {
+            Product product = existingProduct.get();
+            product.setName(dto.getName());
+            product.setDescription(dto.getDescription());
+            product.setPrice(dto.getPrice());
+            product.setStock(dto.getStock());
+            product.setCategory(dto.getCategory());
+            return Optional.of(productRepository.save(product));
+        }
+        return Optional.empty();
+    }
+}
