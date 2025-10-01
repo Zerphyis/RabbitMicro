7c374e8563a6244bbc5239415439d9fd9a916bc0
Otavio <estudosprogotavio@gmail.com>
2025-10-01 01:53:56 -0300

    TASK12 feat:adicionando entidade Users

---
+++ src/main/java/dev/Zerphyis/microRabbitMq/Domain/model/users/Users.java
@@ -0,0 +1,73 @@
+package dev.Zerphyis.microRabbitMq.Domain.model.users;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "usuarios")
+public class Users implements UserDetails {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @NotBlank
+    @Column(name = "nome")
+    private String name;
+
+    @NotBlank
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @NotBlank
+    @Column(name = "senha")
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return name;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
