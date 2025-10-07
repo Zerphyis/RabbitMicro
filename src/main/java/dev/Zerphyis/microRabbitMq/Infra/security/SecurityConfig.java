2533a535cd49b4baa87c02cb40ab0c2cb426ed07
Otavio <estudosprogotavio@gmail.com>
2025-10-07 02:11:48 -0300

    TASK13 feat:adicionando Configurações de Segurança

---
+++ src/main/java/dev/Zerphyis/microRabbitMq/Infra/security/SecurityConfig.java
@@ -0,0 +1,62 @@
+package dev.Zerphyis.microRabbitMq.Infra.security;
+
+import dev.Zerphyis.microRabbitMq.Application.services.user.UserService;
+import dev.Zerphyis.microRabbitMq.Application.useCases.users.LogoutUserUseCase;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SecurityConfig {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final LogoutUserUseCase logoutUserUseCase;
+    private final UserService userService;
+
+    public SecurityConfig(JwtTokenProvider jwtTokenProvider,
+                          LogoutUserUseCase logoutUserUseCase,
+                          UserService userService) {
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.logoutUserUseCase = logoutUserUseCase;
+        this.userService = userService;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/**").permitAll()
+
+                        .requestMatchers("/api/users/**/search").hasRole("ADMIN")
+                        .requestMatchers("/api/users/{id}").hasRole("ADMIN")
+                        .requestMatchers("/api/users/{id}").hasRole("ADMIN")
+
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(
+                        new FilterSecurity(jwtTokenProvider, logoutUserUseCase, userService),
+                        UsernamePasswordAuthenticationFilter.class
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
