f7290bde9ad6615141b61c9b3d65a83e4cedb7ee
Otavio <estudosprogotavio@gmail.com>
2025-10-07 02:07:44 -0300

    TASK13 refactor:retirando @Column de UsersRegisterDto

--- src/main/java/dev/Zerphyis/microRabbitMq/Application/dto/users/UsersRegisterDto.java
+++ src/main/java/dev/Zerphyis/microRabbitMq/Application/dto/users/UsersRegisterDto.java
@@ -1,17 +1,13 @@
 package dev.Zerphyis.microRabbitMq.Application.dto.users;
 
-import jakarta.persistence.Column;
+
+import dev.Zerphyis.microRabbitMq.Domain.model.users.typeRole.TypeRole;
 import lombok.Data;
 
 @Data
 public class UsersRegisterDto {
-    @Column(name = "nome")
     private String name;
-
-    @Column(name = "email", nullable = false)
     private String email;
-
-    @Column(name = "senha")
     private String password;
-
+    private TypeRole role;
 }
