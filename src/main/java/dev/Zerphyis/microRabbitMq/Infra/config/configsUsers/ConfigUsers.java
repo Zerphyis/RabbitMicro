c1f807d66689f9e450b9d8a320e6d45152ad6def
Otavio <estudosprogotavio@gmail.com>
2025-10-07 02:11:27 -0300

    TASK13 feat:adicionando Beans de novos Uses Cases

--- src/main/java/dev/Zerphyis/microRabbitMq/Infra/config/configsUsers/ConfigUsers.java
+++ src/main/java/dev/Zerphyis/microRabbitMq/Infra/config/configsUsers/ConfigUsers.java
@@ -1,12 +1,11 @@
 package dev.Zerphyis.microRabbitMq.Infra.config.configsUsers;
 
-import dev.Zerphyis.microRabbitMq.Application.useCases.users.DeactiveUserUseCase;
-import dev.Zerphyis.microRabbitMq.Application.useCases.users.FindUsersUseCase;
-import dev.Zerphyis.microRabbitMq.Application.useCases.users.RegisterUsersUseCase;
-import dev.Zerphyis.microRabbitMq.Application.useCases.users.UpdateUsersUseCase;
+import dev.Zerphyis.microRabbitMq.Application.useCases.users.*;
 import dev.Zerphyis.microRabbitMq.Domain.repository.usersRepository.UserRepository;
+import dev.Zerphyis.microRabbitMq.Infra.security.JwtTokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
@@ -15,6 +14,16 @@ public class ConfigUsers {
     @Bean
     public RegisterUsersUseCase registerUsersUseCase(UserRepository repository, PasswordEncoder encoder){
         return  new RegisterUsersUseCase(repository,encoder);
+    }
+    @Bean
+    public LoginUserUseCase loginUserUseCase(AuthenticationManager authManager,
+                                             JwtTokenProvider jwtTokenProvider) {
+        return new LoginUserUseCase(authManager, jwtTokenProvider);
+    }
+
+    @Bean
+    public LogoutUserUseCase logoutUserUseCase() {
+        return new LogoutUserUseCase();
     }
 
     @Bean
