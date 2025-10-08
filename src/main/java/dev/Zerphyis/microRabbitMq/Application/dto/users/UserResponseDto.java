e767456418a65318bd1e29d964a78e200ff352de
Otavio <estudosprogotavio@gmail.com>
2025-10-08 14:16:33 -0300

    TASK13 feat:adicionando @allArgsConstructor em UserResponseDto

--- src/main/java/dev/Zerphyis/microRabbitMq/Application/dto/users/UserResponseDto.java
+++ src/main/java/dev/Zerphyis/microRabbitMq/Application/dto/users/UserResponseDto.java
@@ -1,8 +1,10 @@
 package dev.Zerphyis.microRabbitMq.Application.dto.users;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class UserResponseDto {
     private String name;
     private String email;
