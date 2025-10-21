c2897295ebdf2e875b4f6f36b5ddea4391a98ca1
Otavio <estudosprogotavio@gmail.com>
2025-10-21 12:58:40 -0300

    TASK16 refactor:corrigindo erro de clico de dependencia em SecurityConfig

--- src/main/java/dev/Zerphyis/microRabbitMq/Infra/security/SecurityConfig.java
+++ src/main/java/dev/Zerphyis/microRabbitMq/Infra/security/SecurityConfig.java
@@ -1,15 +1,15 @@
 package dev.Zerphyis.microRabbitMq.Infra.security;
 
-import dev.Zerphyis.microRabbitMq.Application.useCases.users.LogoutUserUseCase;
 import dev.Zerphyis.microRabbitMq.Application.services.user.UserService;
+import dev.Zerphyis.microRabbitMq.Application.useCases.users.LoginUserUseCase;
+import dev.Zerphyis.microRabbitMq.Application.useCases.users.LogoutUserUseCase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -18,43 +18,46 @@ public class SecurityConfig {
 
     private final JwtTokenProvider jwtTokenProvider;
     private final LogoutUserUseCase logoutUserUseCase;
+    private final UserService userService;
+    private final LoginUserUseCase loginUserUseCase;
 
     public SecurityConfig(JwtTokenProvider jwtTokenProvider,
-                          LogoutUserUseCase logoutUserUseCase) {
+                          LogoutUserUseCase logoutUserUseCase,
+                          @Lazy UserService userService,
+                          LoginUserUseCase loginUserUseCase) {
         this.jwtTokenProvider = jwtTokenProvider;
         this.logoutUserUseCase = logoutUserUseCase;
+        this.userService = userService;
+        this.loginUserUseCase = loginUserUseCase;
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http,
-                                                   FilterSecurity filterSecurity) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationManager authManager) throws Exception {
+        loginUserUseCase.setAuthenticationManager(authManager);
+
         http
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()
-                        .requestMatchers("/api/users/**/search").hasRole("ADMIN")
-                        .requestMatchers("/api/users/**").hasRole("ADMIN")
+                        .requestMatchers("/api/users/register", "/api/users/login").permitAll()
                         .requestMatchers("/producer/**").permitAll()
+                        .requestMatchers("/api/users/search").hasRole("ADMIN")
+                        .requestMatchers("/api/users/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
-                .addFilterBefore(filterSecurity, UsernamePasswordAuthenticationFilter.class);
+                .userDetailsService(userService)
+                .addFilterBefore(filterSecurity(), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
 
     @Bean
-    public FilterSecurity filterSecurity(UserService userService) {
+    public FilterSecurity filterSecurity() {
         return new FilterSecurity(jwtTokenProvider, logoutUserUseCase, userService);
     }
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
